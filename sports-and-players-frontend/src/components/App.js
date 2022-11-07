d55831872a567a1fdea88722880b441d7375fd02
Christian Castillo <castillochristian3@gmail.com>
2022-11-07 12:14:49 -0500

    added Home component with h1 athletes

--- sports-and-players-frontend/src/components/App.js
+++ sports-and-players-frontend/src/components/App.js
@@ -1,12 +1,16 @@
 import React, { useEffect, useState } from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import NavBar from './NavBar';
+import Home from './Home';
 
 
 function App() {
   return (
     <div>
       <NavBar />
+      <Routes>
+        <Route path='/' element={<Home/>} />
+      </Routes>
     </div>
   );
 }
